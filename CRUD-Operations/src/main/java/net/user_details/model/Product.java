d1d85a7ca027950f92fade603942a5bfbe1c02d5
Tanvi M <tanvimaheshwari67@gmail.com>
2022-08-26 18:05:56 +0530

    added product table

---
+++ CRUD-Operations/src/main/java/net/user_details/model/Product.java
@@ -0,0 +1,47 @@
+package net.user_details.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="product")
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productId;
+
+    @Column(name="product_title",nullable = false)
+    @NotEmpty
+    private String productTitle;
+
+    @Column(name = "category",nullable = false)
+    @NotEmpty
+    private String category;
+
+    @Column(name = "image_URL",nullable = false)
+    private String imgURL;
+
+    @Column(name = "price",nullable = false)
+    @NotEmpty
+    private double price;
+
+    @NotEmpty
+    @Column(name = "seller_id",nullable = false)
+    private String sellerId;
+
+
+
+
+}
---
+++ CRUD-Operations/src/main/java/net/user_details/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package net.user_details.repository;
+
+import net.user_details.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
